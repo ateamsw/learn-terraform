eb0b91ac7e57dd40664637e9f504c0ffa54c28b0
Chris Wiederspan <chris@wiederspan.com>
2020-01-18 14:14:07 -0700

    Added the 6th challenge

---
+++ challenges/06_kubernetes/solution/main.tf
@@ -0,0 +1,74 @@
+terraform {
+  required_version = ">= 0.12"
+  
+  backend "azurerm" {
+    environment = "public"
+  }
+}
+
+provider "azurerm" {
+  version = "=1.41.0"
+}
+
+provider "azuread" {
+  version = "=0.7.0"
+}
+
+# Define Kubernetes provider to use the AKS cluster
+provider "kubernetes" {
+  version = "1.10"
+  host                   = azurerm_kubernetes_cluster.aks.kube_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
+}
+
+variable "name_prefix"       { }
+variable "name_base"         { }
+variable "name_suffix"       { }
+variable "location"          { }
+variable "node_count"        { }
+variable "aks_version"       { }
+
+locals {
+  base_name  = "${var.name_prefix}-${var.name_base}-${var.name_suffix}"
+}
+
+resource "azurerm_resource_group" "group" {
+  name     = "${local.base_name}-rg"
+  location = var.location
+}
+
+module "service_principal" {
+  source    = "./service-principal"
+  base_name = local.base_name
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "${local.base_name}-aks"
+  resource_group_name = azurerm_resource_group.group.name
+  location            = azurerm_resource_group.group.location
+  dns_prefix          = local.base_name
+  kubernetes_version  = var.aks_version
+
+  agent_pool_profile {
+    name    = "linux01"
+    count   = var.node_count
+    vm_size = "Standard_DS2_v2"
+    os_type = "Linux"
+    type    = "VirtualMachineScaleSets"
+  }
+
+  service_principal {
+    client_id     = module.service_principal.client_id
+    client_secret = module.service_principal.client_secret
+  }
+
+  role_based_access_control {
+    enabled = true
+  }
+
+  depends_on = [
+    module.service_principal.client_id
+  ]
+}
---
+++ challenges/06_kubernetes/solution/terraform.tfvars
@@ -0,0 +1,9 @@
+name_prefix = "xyz"
+name_base   = "kubernetes"
+name_suffix = "20200121"
+
+location    = "westus2"
+
+// az aks get-versions -l westus2 -o table
+aks_version = "1.15.7"
+node_count  = "1"
